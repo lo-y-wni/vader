4d86a1c21f2108faffa114769ed11a4d514208f2
svahl991 <vahl@ucar.edu>
2021-04-28 13:52:28 -0600

    Change some trace stmnts to debug

--- src/vader/vader.cc
+++ src/vader/vader.cc
@@ -27,13 +27,13 @@ void Vader::changeVar(atlas::FieldSet * afieldset, const oops::Variables & vars)
 	// Actual changeVar functionality not implemented yet
 	// Demonstrate that VADER has received the model fields via the Atlas fieldset
 	std::string firstFieldName = afieldset->field_names().front();
-	oops::Log::trace() << "First field name in fieldset: " << firstFieldName << std::endl;
+	oops::Log::debug() << "First field name in fieldset: " << firstFieldName << std::endl;
 	atlas::Field field1 = afieldset->field(firstFieldName);
-	oops::Log::trace() << "Size: " << field1.size() << std::endl;
-	oops::Log::trace() << "Rank: " << field1.rank() << std::endl;
+	oops::Log::debug() << "Size: " << field1.size() << std::endl;
+	oops::Log::debug() << "Rank: " << field1.rank() << std::endl;
 	if (field1.rank() == 2 && field1.size() > 0) {
 		auto field1_view = atlas::array::make_view <double , 2>( field1 );
-		oops::Log::trace() << "First data element: " << field1_view(0, 0) << std::endl;
+		oops::Log::debug() << "First data element: " << field1_view(0, 0) << std::endl;
 	}
 
 	oops::Log::trace() << "leaving Vader::changeVar: " << std::endl;
