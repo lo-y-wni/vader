65f2dd8adadc3db609b992cc69fbc2a7babb052e
svahl991 <vahl@ucar.edu>
2021-06-18 20:11:46 +0000

    Coding norms cookbook

--- src/vader/cookbook.h
+++ src/vader/cookbook.h
@@ -5,38 +5,40 @@
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-#ifndef COOKBOOK_H_
-#define COOKBOOK_H_
+#ifndef SRC_VADER_COOKBOOK_H_
+#define SRC_VADER_COOKBOOK_H_
 
 #include <vector>
 #include <unordered_map>
+#include <string>
 
 #include "oops/util/Logger.h"
-#include "vader.h"
-#include "vadervariables.h"
+#include "vader/vader.h"
+#include "vader/vadervariables.h"
 
 // Recipe headers
 #include "recipes/TempToPTemp.h"
 #include "recipes/PressureToDelP.h"
 
-namespace vader {
+namespace vader
+{
 
 // -----------------------------------------------------------------------------
 // This defines the recipes used (and their priority) by default in Vader
 // (Recipes can be added, removed, or rearranged in cookbook by specifying in
 //  yaml.)
 std::unordered_map<std::string, std::vector<std::string>>
-   Vader::getDefaultCookbookDef() {
-   return {
-      // This defines the default Vader Cookbook
-      // Key: name of the variable produced by all the recipes in the Value
-      // Value: a vector of recipe names that will be searched, in order,
-      //        by Vader for viability
-        {VV_PT, {TempToPTemp::Name}}
-      , {VV_DELP, {PressureToDelP::Name}}
-   };
+Vader::getDefaultCookbookDef()
+{
+    return {
+        // This defines the default Vader Cookbook
+        // Key: name of the variable produced by all the recipes in the Value
+        // Value: a vector of recipe names that will be searched, in order,
+        //        by Vader for viability
+        {VV_PT, {TempToPTemp::Name}},
+        {VV_DELP, {PressureToDelP::Name}}};
 }
 
-} // namespace vader
+}  // namespace vader
 
-#endif  // COOKBOOK_H_
+#endif  // SRC_VADER_COOKBOOK_H_
