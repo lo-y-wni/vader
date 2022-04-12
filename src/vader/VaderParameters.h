a86b2ee0bc797e3bb265d97eaf8499f264b2d29b
svahl991 <vahl@ucar.edu>
2022-04-12 13:35:54 -0600

    Clarify Doxygen comment wording

--- src/vader/VaderParameters.h
+++ src/vader/VaderParameters.h
@@ -20,13 +20,14 @@ class VaderParameters : public oops::Parameters {
   OOPS_CONCRETE_PARAMETERS(VaderParameters, Parameters)
 
  public:
-  /// Vader's design is that it should not have any RequiredParameters
+  /// Vader's design is that it should not have any RequiredParameters,
+  /// and neither should the individual Recipes.
   ///
-  /// An optional `recipeParams` parameter, which contains an (optional)
-  /// list of Parameter (wrappers), each of which contains parameters
-  /// which configure one of the individual recipes.
-  /// Recipies should also be designed so that they can be used with a
-  /// default configuration and no parameters.
+  /// 'recipeParams' is an optional Vader parameter which contains a
+  /// a vector of RecipeParametersWrapper objects. Because Recipes should
+  /// be designed without RequiredParameters, these Wrappers are needed
+  /// only for Recipes for which the caller wishes to alter the Recipe's default
+  /// configuration and/or behavior.
   oops::OptionalParameter<std::vector<RecipeParametersWrapper>> recipeParams{
      "recipe parameters",
      "Parameters to configure individual recipe functionality",
