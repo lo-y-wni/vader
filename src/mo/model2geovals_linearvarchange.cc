863554df653eb4e82219372478d1fe71659ba7c6
Stefano Migliorini <64269292+stemiglio@users.noreply.github.com>
2022-09-30 08:11:49 +0100

    Feature/pbar ad test (#81)
    * new file with TL functions for observation operators. New TL function to calculate air pressure increments on theta levels as a function of increments on pressure levels + adjoint of evalAirPressureTL

---
+++ src/mo/model2geovals_linearvarchange.cc
@@ -0,0 +1,62 @@
+/*
+ * (C) Crown Copyright 2022 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "atlas/array/MakeView.h"
+#include "atlas/field/FieldSet.h"
+
+#include "mo/constants.h"
+#include "mo/model2geovals_linearvarchange.h"
+
+using atlas::array::make_view;
+
+
+namespace mo {
+
+void evalAirPressureTL(atlas::FieldSet & incFlds, const atlas::FieldSet & augStateFlds) {
+  // height at pressure levels
+  const auto hlView = make_view<const double, 2>(augStateFlds["height_levels"]);
+  // height at theta levels
+  const auto hView = make_view<const double, 2>(augStateFlds["height"]);
+
+  const auto pIncView = make_view<double, 2>(incFlds["air_pressure_levels"]);
+  auto pbarIncView = make_view<double, 2>(incFlds["air_pressure"]);
+
+  double alpha_jl(0.0);
+
+  atlas::idx_t lvls(incFlds["air_pressure"].levels());
+  for (atlas::idx_t jn = 0; jn < pbarIncView.shape(0); ++jn) {
+    for (atlas::idx_t jl = 0; jl < lvls; ++jl) {
+      alpha_jl = (hView(jn, jl) - hlView(jn, jl)) / (hlView(jn, jl+1) - hlView(jn, jl));
+      pbarIncView(jn, jl) = (1.0 - alpha_jl) * pIncView(jn, jl) +
+                            alpha_jl * pIncView(jn, jl+1);
+    }
+  }
+}
+
+void evalAirPressureAD(atlas::FieldSet & hatFlds, const atlas::FieldSet & augStateFlds) {
+  // height at pressure levels
+  const auto hlView = make_view<const double, 2>(augStateFlds["height_levels"]);
+  // height at theta levels
+  const auto hView = make_view<const double, 2>(augStateFlds["height"]);
+
+  auto pbarHatView = make_view<double, 2>(hatFlds["air_pressure"]);
+  auto pHatView = make_view<double, 2>(hatFlds["air_pressure_levels"]);
+
+  double alpha_jl(0.0);
+
+  atlas::idx_t lvls(hatFlds["air_pressure"].levels());
+  for (atlas::idx_t jn = 0; jn < pHatView.shape(0); ++jn) {
+    for (atlas::idx_t jl = 0; jl < lvls; ++jl) {
+      alpha_jl = (hView(jn, jl) - hlView(jn, jl)) / (hlView(jn, jl+1) - hlView(jn, jl));
+      pHatView(jn, jl) += (1.0 - alpha_jl) * pbarHatView(jn, jl);
+      pHatView(jn, jl+1) += alpha_jl * pbarHatView(jn, jl);
+      pbarHatView(jn, jl) = 0.0;
+    }
+  }
+}
+
+}  // namespace mo
--- src/mo/model2geovals_linearvarchange.h
+++ src/mo/model2geovals_linearvarchange.h
@@ -6,3 +6,21 @@
  */
 
 #pragma once
+
+#include "atlas/field/FieldSet.h"
+
+namespace mo {
+
+/// \details This calculates the air pressure increment fields on theta (full) levels
+///          from vertical interpolation of air pressure increment fields
+///          on pressure (half) levels:
+///          p'(i) = ((h(i) - h(i-1/2))p'(i+1/2) + (h(i+1/2) - h(i))p'(i-1/2))
+///                  / (h(i+1/2) - h(i-1/2))
+void evalAirPressureTL(atlas::FieldSet & incFlds, const atlas::FieldSet & augStateFlds);
+
+/// \details This is the adjoint of the calculation to create the air pressure increment fields
+///          on theta (full) levels from vertical interpolation of air pressure increment fields
+///          on pressure (half) levels.
+void evalAirPressureAD(atlas::FieldSet & hatFlds, const atlas::FieldSet & augStateFlds);
+
+}  // namespace mo
