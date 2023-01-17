3d0624478f7850ab68d4aa62a328defeadc4359b
Stefano Migliorini <64269292+stemiglio@users.noreply.github.com>
2023-01-17 09:39:15 +0000

    TL and AD transforms for Hofx (#131)
    * TL and AD of rh, rh2, pstar and surface temperature.

--- src/mo/model2geovals_linearvarchange.cc
+++ src/mo/model2geovals_linearvarchange.cc
@@ -87,4 +87,103 @@ void evalSurfWindAD(atlas::FieldSet & hatFlds) {
   }
 }
 
+void evalRelativeHumidityTL(atlas::FieldSet & incFlds, const atlas::FieldSet & augStateFlds) {
+  const auto qIncView = make_view<double, 2>(incFlds["specific_humidity"]);
+  const auto temperIncView = make_view<double, 2>(incFlds["air_temperature"]);
+  auto rhIncView = make_view<double, 2>(incFlds["relative_humidity"]);
+
+  // qsat and dlnesdT are calculated by evalSatVaporPressure and evalSatSpecificHumidity
+  // from air_temperature and air_pressure inputs
+  const auto qView = make_view<const double, 2>(augStateFlds["specific_humidity"]);
+  const auto qsatView = make_view<const double, 2>(augStateFlds["qsat"]);
+  const auto dlnesdT = make_view<const double, 2>(augStateFlds["dlsvpdT"]);
+
+  atlas::idx_t lvls(incFlds["relative_humidity"].levels());
+  for (atlas::idx_t jn = 0; jn < rhIncView.shape(0); ++jn) {
+    for (atlas::idx_t jl = 0; jl < lvls; ++jl) {
+      rhIncView(jn, jl) = 100.0 * (qIncView(jn, jl) - qView(jn, jl) * dlnesdT(jn, jl)
+                          * temperIncView(jn, jl)) / qsatView(jn, jl);
+    }
+  }
+}
+
+void evalRelativeHumidityAD(atlas::FieldSet & hatFlds, const atlas::FieldSet & augStateFlds) {
+  auto qHatView = make_view<double, 2>(hatFlds["specific_humidity"]);
+  auto temperHatView = make_view<double, 2>(hatFlds["air_temperature"]);
+  auto rhHatView = make_view<double, 2>(hatFlds["relative_humidity"]);
+
+  // qsat and dlnesdT are calculated by evalSatVaporPressure and evalSatSpecificHumidity
+  // from air_temperature and air_pressure inputs
+  const auto qView = make_view<const double, 2>(augStateFlds["specific_humidity"]);
+  const auto qsatView = make_view<const double, 2>(augStateFlds["qsat"]);
+  const auto dlnesdT = make_view<const double, 2>(augStateFlds["dlsvpdT"]);
+
+  atlas::idx_t lvls(hatFlds["relative_humidity"].levels());
+  for (atlas::idx_t jn = 0; jn < rhHatView.shape(0); ++jn) {
+    for (atlas::idx_t jl = 0; jl < lvls; ++jl) {
+      qHatView(jn, jl) += 100.0 * rhHatView(jn, jl) / qsatView(jn, jl);
+      temperHatView(jn, jl) -= 100.0 * qView(jn, jl) * dlnesdT(jn, jl) * rhHatView(jn, jl)
+                            / qsatView(jn, jl);
+      rhHatView(jn, jl) = 0.0;
+    }
+  }
+}
+
+void evalRelativeHumidity_2mTL(atlas::FieldSet & incFlds) {
+  const auto rhIncView = make_view<double, 2>(incFlds["relative_humidity"]);
+  auto rh2mIncView = make_view<double, 2>(incFlds["relative_humidity_2m"]);
+
+  for (atlas::idx_t jn = 0; jn < rh2mIncView.shape(0); ++jn) {
+      rh2mIncView(jn, 0) = rhIncView(jn, 0);
+  }
+}
+
+void evalRelativeHumidity_2mAD(atlas::FieldSet & HatFlds) {
+  auto rhHatView = make_view<double, 2>(HatFlds["relative_humidity"]);
+  auto rh2mHatView = make_view<double, 2>(HatFlds["relative_humidity_2m"]);
+
+  for (atlas::idx_t jn = 0; jn < rh2mHatView.shape(0); ++jn) {
+    rh2mHatView(jn, 0) += rhHatView(jn, 0);
+    rhHatView(jn, 0) = 0.0;
+  }
+}
+
+void evalSurfacePressureTL(atlas::FieldSet & incFlds) {
+  const auto pIncView = make_view<double, 2>(incFlds["air_pressure"]);
+  auto pstarIncView = make_view<double, 2>(incFlds["surface_pressure"]);
+
+  for (atlas::idx_t jn = 0; jn < pstarIncView.shape(0); ++jn) {
+      pstarIncView(jn, 0) = pIncView(jn, 0);
+  }
+}
+
+void evalSurfacePressureAD(atlas::FieldSet & HatFlds) {
+  auto pHatView = make_view<double, 2>(HatFlds["air_pressure"]);
+  auto pstarHatView = make_view<double, 2>(HatFlds["surface_pressure"]);
+
+  for (atlas::idx_t jn = 0; jn < pstarHatView.shape(0); ++jn) {
+    pstarHatView(jn, 0) += pHatView(jn, 0);
+    pHatView(jn, 0) = 0.0;
+  }
+}
+
+void evalSurfaceTemperatureTL(atlas::FieldSet & incFlds) {
+  const auto temperIncView = make_view<double, 2>(incFlds["air_temperature"]);
+  auto surfTemperIncView = make_view<double, 2>(incFlds["surface_temperature"]);
+
+  for (atlas::idx_t jn = 0; jn < surfTemperIncView.shape(0); ++jn) {
+      surfTemperIncView(jn, 0) = temperIncView(jn, 0);
+  }
+}
+
+void evalSurfaceTemperatureAD(atlas::FieldSet & HatFlds) {
+  auto temperHatView = make_view<double, 2>(HatFlds["air_temperature"]);
+  auto surfTemperHatView = make_view<double, 2>(HatFlds["surface_temperature"]);
+
+  for (atlas::idx_t jn = 0; jn < surfTemperHatView.shape(0); ++jn) {
+    surfTemperHatView(jn, 0) += temperHatView(jn, 0);
+    temperHatView(jn, 0) = 0.0;
+  }
+}
+
 }  // namespace mo
--- src/mo/model2geovals_linearvarchange.h
+++ src/mo/model2geovals_linearvarchange.h
@@ -27,8 +27,42 @@ void evalAirPressureAD(atlas::FieldSet & hatFlds, const atlas::FieldSet & augSta
 ///          wind increment at the lowest level
 void evalSurfWindTL(atlas::FieldSet & incFlds);
 
-/// \details This calculates the adjoint of the wind increment at 10 m from the adjoint
-///          of the atmospheric wind at the lowest level
+/// \details This calculates the adjoint of the wind increment at the lowest level from the adjoint
+///          of the atmospheric wind at 10 m
 void evalSurfWindAD(atlas::FieldSet & hatFlds);
+
+/// \details This calculates the relative humidity increment from the air
+///          temperature and humidity increment as:
+///          rh' ~ (q'/qs - (q/qs) dlnesdT T')*100
+///          This assumes air pressure increments can be neglected (usual approximation)
+void evalRelativeHumidityTL(atlas::FieldSet & incFlds, const atlas::FieldSet & augStateFlds);
+
+/// \details This calculates the adjoint of the air temperature and humidity
+///          from the adjoint of the relative humidity
+void evalRelativeHumidityAD(atlas::FieldSet & hatFlds, const atlas::FieldSet & augStateFlds);
+
+/// \details This calculates the relative humidity increment at 2 m from the atmospheric
+///          relative humidity increment at the lowest level
+void evalRelativeHumidity_2mTL(atlas::FieldSet & incFlds);
+
+/// \details This calculates the adjoint of the relative humidity
+///          at the lowest level from the adjoint of the atmospheric relative humidity at 2 m
+void evalRelativeHumidity_2mAD(atlas::FieldSet & HatFlds);
+
+/// \details This calculates the surface pressure increment from the atmospheric
+///          pressure increment at the lowest level
+void evalSurfacePressureTL(atlas::FieldSet & incFlds);
+
+/// \details This calculates the adjoint of the atmospheric pressure
+///          at the lowest level from the adjoint of the surface pressure
+void evalSurfacePressureAD(atlas::FieldSet & HatFlds);
+
+/// \details This calculates the surface temperature increment from the atmospheric
+///          temperature increment at the lowest level
+void evalSurfaceTemperatureTL(atlas::FieldSet & incFlds);
+
+/// \details This calculates the adjoint of the atmospheric temperature
+///          at the lowest level from the adjoint of the surface temperature
+void evalSurfaceTemperatureAD(atlas::FieldSet & HatFlds);
 
 }  // namespace mo
