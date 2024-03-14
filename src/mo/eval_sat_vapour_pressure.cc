3afcf7cd7450af2b1f5165d606dd58b26d8f88d4
Lorenzo Milazzo <82884956+mo-lormi@users.noreply.github.com>
2024-03-14 21:34:01 +0000

    redefined the NL variable transform for 'saturation vapour pressure' (#239)
    * redefined the Variable Transform for 'saturation vapour pressure'
    
    * update 12.03.24-01
    
    * rename functions
    
    * trigger CI
    
    * trigger CI
    
    * trigger CI (rebuilding build cache)
    
    ---------
    
    Co-authored-by: shlyaeva <anna.v.shlyaeva@noaa.gov>

--- src/mo/eval_sat_vapour_pressure.cc
+++ src/mo/eval_sat_vapour_pressure.cc
@@ -6,10 +6,6 @@
  */
 
 
-#include <algorithm>
-#include <functional>
-#include <memory>
-#include <ostream>
 #include <string>
 #include <vector>
 
@@ -20,18 +16,15 @@
 #include "mo/functions.h"
 #include "mo/lookups.h"
 
-#include "oops/base/Variables.h"
 #include "oops/util/Logger.h"
 
-using atlas::array::make_view;
-using atlas::idx_t;
 
 namespace mo {
 
-void eval_sat_vapour_pressure_nl(atlas::FieldSet & fields)
-{
-  oops::Log::trace() << "[eval_sat_vapour_pressure_nl()] starting ..." << std::endl;
 
+void eval_sat_vapour_pressure_variant_nl(const std::string field_out,
+                                         atlas::FieldSet & fields)
+{
   // normalised T lambda function
   // which enforces upper and lower bounds on T
   auto normalisedT = [](const double tVal) {
@@ -60,39 +53,52 @@ void eval_sat_vapour_pressure_nl(atlas::FieldSet & fields)
      return (weight * v2 + (1 - weight) * v1);
   };
 
-  // lambda function to determine the gradient of variable in table
-  // with respect to temperature
-  auto normalisedDiff = [](const double v1, const double v2) {
-     return (v2 - v1)/ constants::Tinc;
-  };
-
-  double w;  // weight
-  std::size_t indx;  // index
-
-  const auto tView  = make_view<const double, 2>(fields["air_temperature"]);
-
-  const std::vector<std::string> fnames {"svp", "dlsvpdT"};
-  int ival = 0;  // set ival = 2 to get svp wrt water
-  for (auto & ef : fnames) {
-    if (fields.has(ef)) {
-      // check this recipe to calculate svp is correct
-      const auto& lookup = lookups::allSvpLookupTable[ival];
-      ++ival;
-
-      atlas::field::for_each_value(atlas::execution::par_unseq,
-                                   fields["air_temperature"],
-                                   fields[ef],
-                                   [&](const double T,
-                                       double& svp) {
-        indx = index(normalisedT(T));
-        w = weight(normalisedT(T));
-        svp = interp(w, lookup.at(indx), lookup.at(indx+1));
-      });
-    }
+  // ID to identify a type of 'saturation water pressure'
+  int ival;
+  if (field_out == "svp") {
+    ival = 0;
+  } else if (field_out == "dlsvpdT") {
+    ival = 1;
   }
 
-  oops::Log::trace() << "[eval_sat_vapour_pressure_nl()] ... exit" << std::endl;
+  // weight
+  double w;
+  // index
+  std::size_t indx;
+
+  const auto& lookup = lookups::allSvpLookupTable[ival];
+
+  atlas::field::for_each_value(
+    atlas::execution::par_unseq,
+    fields["air_temperature"],
+    fields[field_out],
+    [&](const double air_temp, double& svp) {
+      indx = index(normalisedT(air_temp));
+      w = weight(normalisedT(air_temp));
+      svp = interp(w, lookup.at(indx), lookup.at(indx+1));
+    });
 }
+
+
+void eval_sat_vapour_pressure_nl(atlas::FieldSet & fields)
+{
+  oops::Log::trace() << "[eval_sat_vapour_pressure_svp_nl()] starting ..." << std::endl;
+
+  eval_sat_vapour_pressure_variant_nl("svp", fields);
+
+  oops::Log::trace() << "[eval_sat_vapour_pressure_svp_nl()] ... done" << std::endl;
+}
+
+
+void eval_derivative_ln_svp_wrt_temperature_nl(atlas::FieldSet & fields)
+{
+  oops::Log::trace() << "[eval_sat_vapour_pressure_dlsvpdT_nl()] starting ..." << std::endl;
+
+  eval_sat_vapour_pressure_variant_nl("dlsvpdT", fields);
+
+  oops::Log::trace() << "[eval_sat_vapour_pressure_dlsvpdT_nl()] ... done" << std::endl;
+}
+
 
 }  // namespace mo
 
--- src/mo/eval_sat_vapour_pressure.h
+++ src/mo/eval_sat_vapour_pressure.h
@@ -1,5 +1,5 @@
 /*
- * (C) Crown Copyright 2023 Met Office
+ * (C) Crown Copyright 2023-2024 Met Office
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -8,17 +8,27 @@
 #pragma once
 
 #include <string>
-#include <vector>
 
-#include "atlas/field/Field.h"
 #include "atlas/field/FieldSet.h"
+
 
 namespace mo {
 
-/// \brief function to evaluate saturation water pressure (svp) [Pa]
-/// the Atlas field in the argument must contain an inizialised air temperature field
-/// and to have a defined svp field which is then calculated and returned as output
+
+/// \brief function to evaluate the several types of 'saturation water pressure'
 ///
+///  input: Atlas data structure (FieldSet) containing 'air temperature'
+/// output: 'svp' or 'dlsvpdT'
+///
+void eval_sat_vapour_pressure_variant_nl(const std::string field_out,
+                                         atlas::FieldSet & fields);
+
+/// \brief function to evaluate the 'saturation water pressure (svp)'
 void eval_sat_vapour_pressure_nl(atlas::FieldSet & fields);
+
+/// \brief function to evaluate the 'derivative of ln water vapor partial pressure
+/// assuming saturation at interface wrt air temperature at interface (dlsvpdT)'
+void eval_derivative_ln_svp_wrt_temperature_nl(atlas::FieldSet & fields);
+
 
 }  // namespace mo
