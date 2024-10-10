9ea9ff45a17fcf2bd63018ad5315c083fd780991
Steven Sandbach <steven.sandbach@metoffice.gov.uk>
2024-10-10 17:06:36 +0100

    Model variable naming sprint: part 6 (#279)

--- src/mo/eval_surface_temperature.cc
+++ src/mo/eval_surface_temperature.cc
@@ -23,14 +23,14 @@ namespace mo {
 void eval_surface_temperature_nl(atlas::FieldSet & stateFlds) {
   oops::Log::trace() << "[eval_surface_temperature_nl()] starting ..." << std::endl;
   const auto temperView = make_view<double, 2>(stateFlds["air_temperature"]);
-  auto surfTemperView = make_view<double, 2>(stateFlds["surface_temperature"]);
+  auto surfTemperView = make_view<double, 2>(stateFlds["air_temperature_at_2m"]);
   const idx_t sizeOwned =
-    util::getSizeOwned(stateFlds["surface_temperature"].functionspace());
+    util::getSizeOwned(stateFlds["air_temperature_at_2m"].functionspace());
 
   atlas_omp_parallel_for(idx_t jn = 0; jn < sizeOwned; ++jn) {
     surfTemperView(jn, 0) = temperView(jn, 0);
   }
-  stateFlds["surface_temperature"].set_dirty();
+  stateFlds["air_temperature_at_2m"].set_dirty();
 
   oops::Log::trace() << "[eval_surface_temperature_nl()] ... done" << std::endl;
 }
@@ -38,30 +38,30 @@ void eval_surface_temperature_nl(atlas::FieldSet & stateFlds) {
 void eval_surface_temperature_tl(atlas::FieldSet & incFlds) {
   oops::Log::trace() << "[eval_surface_temperature_tl()] starting ..." << std::endl;
   const auto temperIncView = make_view<double, 2>(incFlds["air_temperature"]);
-  auto surfTemperIncView = make_view<double, 2>(incFlds["surface_temperature"]);
+  auto surfTemperIncView = make_view<double, 2>(incFlds["air_temperature_at_2m"]);
   const idx_t sizeOwned =
-    util::getSizeOwned(incFlds["surface_temperature"].functionspace());
+    util::getSizeOwned(incFlds["air_temperature_at_2m"].functionspace());
 
   atlas_omp_parallel_for(idx_t jn = 0; jn < sizeOwned; ++jn) {
       surfTemperIncView(jn, 0) = temperIncView(jn, 0);
   }
-  incFlds["surface_temperature"].set_dirty();
+  incFlds["air_temperature_at_2m"].set_dirty();
   oops::Log::trace() << "[eval_surface_temperature_tl()] ... done" << std::endl;
 }
 
 void eval_surface_temperature_ad(atlas::FieldSet & hatFlds) {
   oops::Log::trace() << "[eval_surface_temperature_ad()] starting ..." << std::endl;
   auto temperHatView = make_view<double, 2>(hatFlds["air_temperature"]);
-  auto surfTemperHatView = make_view<double, 2>(hatFlds["surface_temperature"]);
+  auto surfTemperHatView = make_view<double, 2>(hatFlds["air_temperature_at_2m"]);
   const idx_t sizeOwned =
-    util::getSizeOwned(hatFlds["surface_temperature"].functionspace());
+    util::getSizeOwned(hatFlds["air_temperature_at_2m"].functionspace());
 
   atlas_omp_parallel_for(idx_t jn = 0; jn < sizeOwned; ++jn) {
     temperHatView(jn, 0) += surfTemperHatView(jn, 0);
     surfTemperHatView(jn, 0) = 0.0;
   }
   hatFlds["air_temperature"].set_dirty();
-  hatFlds["surface_temperature"].set_dirty();
+  hatFlds["air_temperature_at_2m"].set_dirty();
 
   oops::Log::trace() << "[eval_surface_temperature_ad()] ... done" << std::endl;
 }
